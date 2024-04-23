846ffb98819d3f81100e9de1bcc0ce0875d2d093
PedroMakengo <pedromakengomiguel@gmail.com>
2024-04-23 20:40:21 +0100

    Fazendo o mapeamento de caminho/pastas

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export function Groups() {
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      <Text>Groups</Text>
+    </View>
+  )
+}
