b5b56065a558c6d2789a29422b5018c2b23395f0
PedroMakengo <pedromakengomiguel@gmail.com>
2024-04-24 10:55:42 +0100

    Adicionado o componente HighLigth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { Logo, Container } from './styles'
+import { Logo, Container, BackIcon, BackButton } from './styles'
 import logoImg from '@assets/logo.png'
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean
+}
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   )
