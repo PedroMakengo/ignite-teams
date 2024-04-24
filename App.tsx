c1ac329f30b3d8cd0c6dc8b3986ba15285db9e03
PedroMakengo <pedromakengomiguel@gmail.com>
2024-04-24 21:36:52 +0100

    Criado a nova tela de criação de grupo

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import { Loading } from '@components/Loading'
 import theme from './src/theme'
 
 import { Groups } from '@screens/Groups'
+import { NewGroup } from '@screens/NewGroup'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +26,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   )
 }
